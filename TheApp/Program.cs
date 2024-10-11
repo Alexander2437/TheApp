78ff46dbfacca4f5117339ef09f553cba44b89d1
Alexander2437 <alexander.vtyurin@gmail.com>
2024-10-11 22:34:04 +0700

    Add project files.

---
+++ TheApp/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace TheApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string MyName = "Александр";
+            byte MyAge = 36;
+            var MyWeight = 80;
+            var MyShoeSize = 41;
+            Console.WriteLine("Привет, мир");
+            Console.WriteLine();
+            Console.WriteLine("Меня зовут " + MyName);
+            Console.WriteLine();
+            Console.WriteLine("Моя фамилия - {0}", "Втюрин");
+            Console.WriteLine();
+            Console.WriteLine($"Мой возраст - {MyAge}");
+            Console.WriteLine();
+            Console.WriteLine("Мой вес - {0}.\n\nМой размер обуви - {1}.", MyWeight, MyShoeSize);
+            Console.Write("\nPlaying ");
+            Console.Write("Checking\n");
+
+            string name = "Jane";
+            byte age = 27;
+            string favcolor = "black";
+            Console.WriteLine("\n{0}\n  {1}\n {2}", name, age, favcolor);*/
+
+            string str = Console.ReadLine();
+            Console.WriteLine(str);
+        }
+    }
+}
