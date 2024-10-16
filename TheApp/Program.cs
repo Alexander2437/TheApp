bd459510f6098a8819f483a0bfd059825d90dc69
Alexander2437 <alexander.vtyurin@gmail.com>
2024-10-16 15:50:54 +0700

    Тема 4.1.18. Условная конструкция

--- TheApp/Program.cs
+++ TheApp/Program.cs
@@ -1,32 +1,35 @@
-﻿namespace TheApp
+﻿using System;
+
+class MainClass
 {
-    internal class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.WriteLine("Напишите свой любимый цвет на английском языке с маленькой буквы");
+
+        var color = Console.ReadLine();
+
+        if (color == "red")
         {
-            /*string MyName = "Александр";
-            byte MyAge = 36;
-            var MyWeight = 80;
-            var MyShoeSize = 41;
-            Console.WriteLine("Привет, мир");
-            Console.WriteLine();
-            Console.WriteLine("Меня зовут " + MyName);
-            Console.WriteLine();
-            Console.WriteLine("Моя фамилия - {0}", "Втюрин");
-            Console.WriteLine();
-            Console.WriteLine($"Мой возраст - {MyAge}");
-            Console.WriteLine();
-            Console.WriteLine("Мой вес - {0}.\n\nМой размер обуви - {1}.", MyWeight, MyShoeSize);
-            Console.Write("\nPlaying ");
-            Console.Write("Checking\n");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
 
-            string name = "Jane";
-            byte age = 27;
-            string favcolor = "black";
-            Console.WriteLine("\n{0}\n  {1}\n {2}", name, age, favcolor);*/
+            Console.WriteLine("Your color is red!");
+        }
 
-            string str = Console.ReadLine();
-            Console.WriteLine(str);
+        else if (color == "green")
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is green");
+        }
+
+        else 
+        {
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("We theink that cyan is better!");
         }
     }
 }
