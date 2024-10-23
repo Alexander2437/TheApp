2ba858afbaa6d91d5e6d8afdfb956c92a3a86d1b
Alexander2437 <alexander.vtyurin@gmail.com>
2024-10-23 21:57:33 +0700

    Задание 4.3.11. Двумерный массив . Вывод на дисплей. Строки и колонки.

--- TheApp/Program.cs
+++ TheApp/Program.cs
@@ -6,19 +6,34 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
-            int[,] array = { { 1, 2, 3 }, { 4, 5, 6 } };
-
-            Console.Write("Количество строк: ");
-            Console.WriteLine(array.GetUpperBound(0) + 1);
-
-            Console.Write("Количество колонок: ");
-            Console.WriteLine(array.GetUpperBound(1) + 1);
-
-            /*foreach (var item in array) 
+            int[,] array = { { 1, 2, 3, }, { 4, 5, 6, }, {7, 8, 9 }, {10, 11, 12} };
+ 
+            /*foreach (int i in array) 
             {
-                Console.Write(item + " ");
+                Console.Write(i + " ");
             }*/
+            
+            //Console.WriteLine(array.Length);
+            
+            //int lines = array.GetUpperBound(0) + 1;
+            //int columns = array.GetUpperBound(1) + 1;
 
+            //Console.WriteLine("Количество строк: {0}", lines);
+            //Console.WriteLine(array.GetUpperBound(0) + 1 + " ");
+
+            //Console.Write("Количество стобцов: {0}", columns);
+            //Console.Write(array.GetUpperBound(1) +1 + " ");
+
+            Console.WriteLine();
+
+            for (int i = 0; i < array.GetUpperBound(0) + 1; i++) 
+            {
+                for (int j = 0; j < array.GetUpperBound(1) + 1; j++) 
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
             Console.ReadKey();
         }
     }
