2f3750919028001691d87a7e5255f6e3bb6a9136
Alexander2437 <alexander.vtyurin@gmail.com>
2024-11-03 14:40:18 +0700

    Тема 5.1.14.

--- TheApp/Program.cs
+++ TheApp/Program.cs
@@ -1,51 +1,51 @@
 ﻿using System;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
-class MainClass
+class Program
 {
     public static void Main(string[] args)
     {
-        (string name, string surname, string login, int LoginLength, string password, bool PetIsPresent, int age, string[] FavColors) user;
+        string[] favcolors = new string[3];
 
-        for (int j = 0; j < 3; j++)
+        for (int i = 0; i < favcolors.Length; i++) 
         {
-            Console.Write("Введите имя пользователя: ");
-            user.name = Console.ReadLine();
-
-            Console.Write("Введиде фамилию пользователя: ");
-            user.surname = Console.ReadLine();
-
-            Console.Write("Придумайте логин: ");
-            user.login = Console.ReadLine();
-            user.LoginLength = user.login.Length;
-
-            Console.WriteLine("Длина логина: {0}", user.LoginLength);
-
-            Console.Write("Придумайте пароль: ");
-            user.password = Console.ReadLine();
-
-            Console.Write("Наличие питомца. Укажите \u0022да\u0022 или \u0022нет\u0022: ");
-            var Answer = Console.ReadLine();
-            if (Answer == "да")
-            {
-                user.PetIsPresent = true;
-                //Console.WriteLine("У вас есть питомец.");
-            }
-            else
-            {
-                user.PetIsPresent = false;
-                //Console.WriteLine("У вас нет пиомца.");
-            }
-
-            Console.Write("Укажите ваш возраст: ");
-            user.age = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Укажите три своих любимых цвета: ");
-            user.FavColors = new string[3];
-            for (int i = 0; i < user.FavColors.Length; i++)
-            {
-                user.FavColors[i] = Console.ReadLine();
-            }
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы: ");
+            favcolors[i] = ShowColor();
         }
+    }
+
+    static string ShowColor()
+    {
+        var color = Console.ReadLine();
+
+        switch (color)
+        {
+            case "red":
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red!");
+                break;
+
+            case "green":
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is green!");
+                break;
+            case "cyan":
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is cyan!");
+                break;
+            default:
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                Console.WriteLine("Your color is yellow!");
+                break;
+        }
+        return color;
     }
 }
