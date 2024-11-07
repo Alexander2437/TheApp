06e4ad3529549d34becf8f9bca64137750113d0d
Alexander2437 <alexander.vtyurin@gmail.com>
2024-11-07 09:02:55 +0700

    Тема 5.2.17 - 5.2.18. Метод ShowArray.

--- TheApp/Program.cs
+++ TheApp/Program.cs
@@ -1,77 +1,83 @@
 ﻿using System;
 
-class MainClass
+class Program
 {
-
-    static string ShowColor(string username, int userage)
-    {
-        Console.WriteLine($"{username}, {userage} лет,\nнапишите свой любимый цвет на английском с маленькой буквы.");
-        var color = Console.ReadLine();
-
-        switch (color)
-        {
-            case "red":
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.Black;
-
-                Console.WriteLine("Your color is red!");
-                break;
-
-            case "green":
-                Console.BackgroundColor = ConsoleColor.Green;
-                Console.ForegroundColor = ConsoleColor.Black;
-
-                Console.WriteLine("Your color is green!");
-                break;
-            case "cyan":
-                Console.BackgroundColor = ConsoleColor.Cyan;
-                Console.ForegroundColor = ConsoleColor.Black;
-
-                Console.WriteLine("Your color is cyan!");
-                break;
-            default:
-                Console.BackgroundColor = ConsoleColor.Yellow;
-                Console.ForegroundColor = ConsoleColor.Red;
-
-                Console.WriteLine("Wrong! Try again!");
-                break;
-        }
-        return color;
-    }
-
     public static void Main(string[] args)
     {
+        int[] array = GetArrayFromConsole(10);
+        ShowArray(array, true);
+    }
 
-        var (name, age) = ("Евгения", 27);
-
-        Console.WriteLine("Мое имя: {0}", name);
-        Console.WriteLine("Мой возраст: {0}", age);
-
-        Console.Write("Введите имя: ");
-        name = Console.ReadLine();
-        Console.Write("Введите возрас с цифрами:");
-        age = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine("Ваше имя: {0}", name);
-        Console.WriteLine("Ваш возраст: {0}", age);
-
-        var array = new string[3];
-        for (int i = 0; i < array.Length; i++)
+    static void ShowArray(int[] array, bool IsSort = false) 
+    {
+        var temp = array;
+        if (IsSort == true) 
         {
-            array[i] = ShowColor(name, age);
+            temp = SortArray(array);
         }
 
-        ShowColors(array);
-
-        //foreach (var color in array)
-            //Console.WriteLine(color);
-    }
-    static void ShowColors(string[] favcolors)
-    {
-        Console.WriteLine("Ваши любимые цвета:");
-        foreach (var color in favcolors)
+        foreach (int item in temp)
         {
-            Console.WriteLine(color);
+            Console.Write(item + " ");
+        }
+    } 
+
+    static int[] GetArrayFromConsole(int num = 5)
+    {
+        var arraytoobtain = new int[num];
+
+        for (int i = 0; i < arraytoobtain.Length; i++)
+        {
+            Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+            arraytoobtain[i] = int.Parse(Console.ReadLine());
+        }
+
+        //SortArray(arraytoobtain);
+
+        return arraytoobtain;
+    }
+
+    static int[] SortArray(int[] arraytosort)
+    {
+        int temp = 0;
+
+        for (int i = 0; i < arraytosort.Length; i++)
+            for (int t = i + 1; t < arraytosort.Length; t++)
+            {
+                if (arraytosort[i] > arraytosort[t])
+                {
+                    temp = arraytosort[i];
+                    arraytosort[i] = arraytosort[t];
+                    arraytosort[t] = temp;
+                }
+            }
+
+        //PrintArray(arraytosort);
+
+        return arraytosort;
+    }
+
+    static int[] PrintArray(int[] arraytoprint)
+    {
+        foreach (int item in arraytoprint)
+            Console.Write(item + "\t");
+
+        return arraytoprint;
+    }
+
+    static void SortComplexArray(int[,] arr)
+    {
+        int temp;
+        for (int i = 0; i <= arr.GetUpperBound(0); i++)
+        {
+            for (int j = 0; j <= arr.GetUpperBound(1); j++)
+                for (int k = j + 1; k <= arr.GetUpperBound(1); k++)
+                    if (arr[i, j] > arr[i, k])
+                    {
+                        temp = arr[i, k];
+                        arr[i, k] = arr[i, j];
+                        arr[i, j] = temp;
+                    }
         }
     }
 }
