7513675766a8cfbbbad2ddafd1568b8a4336b49a
Alexander2437 <alexander.vtyurin@gmail.com>
2024-11-09 20:11:56 +0700

    Передача параметров метода.

--- TheApp/Program.cs
+++ TheApp/Program.cs
@@ -5,21 +5,21 @@ class Program
 {
     public static void Main(string[] args)
     {
-        int vysota;
-        int shyrina;
+        int h;
+        int w;
 
         Console.Write("Укажите длину прямоугольника: ");
-        shyrina = int.Parse(Console.ReadLine());
+        w = int.Parse(Console.ReadLine());
         Console.Write("Укажите высоту прямоугольника: ");
-        vysota = int.Parse(Console.ReadLine());
+        h = int.Parse(Console.ReadLine());
        
-        GetRectangleData(shyrina, vysota, out int area, out int perimetr);
+        GetRectangleData(w, h, out int area, out int perimetr);
         
         Console.WriteLine("Площадь прямоугольника: {0}", area);
         Console.WriteLine("Периметр прямоугольника: {0}", perimetr);
     }
 
-    static void GetRectangleData(int width, int hight, out int RectArea, out int RectPerimetr)
+    static void GetRectangleData(in int width, in int hight, out int RectArea, out int RectPerimetr)
     {
         width++;
         hight++;
