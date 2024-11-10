ef6c94128392d53eb215052d4d5bb72ba37f526d
Alexander2437 <alexander.vtyurin@gmail.com>
2024-11-10 15:36:27 +0700

    Задание 5.5.8. Рекурсивная функция. Возведение числа в степень.

--- TheApp/Program.cs
+++ TheApp/Program.cs
@@ -2,39 +2,31 @@
 
 class Program
 {
-    static int num1 = 667;
     public static void Main(string[] args)
     {
-        Console.WriteLine("Напишите что-то");
-        var str = Console.ReadLine();
+        Console.Write("Введите основание: ");
+        int num = int.Parse(Console.ReadLine());
+        Console.Write("Введите показатель степени: ");
+        byte power = byte.Parse(Console.ReadLine());
 
-        Console.WriteLine("Укажите глубину эха");
-        var deep = int.Parse(Console.ReadLine());
-
-        Echo(str, deep);
-
-        Console.ReadKey();
-
-        /*string s = "abc---def";
-
-        Console.WriteLine("Index: 012345678");
-        Console.WriteLine("1)     {0}", s);
-        Console.WriteLine("2)     {0}", s.Remove(6));
-        Console.WriteLine("3)     {0}", s.Remove(0, 2));*/
+        Console.WriteLine($"Число {num} в степени {power} равно: {PowerUp(num, power)}");
     }
-
-    static void Echo(string saidword, int deep)
+    private static int PowerUp(int N, byte pow)
     {
-        var modif = saidword;
-
-        if (modif.Length > 2)
-            modif = modif.Remove(0, 2);
-        
-        Console.BackgroundColor = ConsoleColor.Red;
-        Console.ForegroundColor = ConsoleColor.Black;
-        Console.WriteLine("..." + modif);
-
-        if (deep > 1)
-            Echo(modif, deep - 1);
+        if (pow == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            if (pow == 1)
+            {
+                return N;
+            }
+            else
+            {
+                return N * PowerUp(N, --pow);
+            }
+        }
     }
 }
