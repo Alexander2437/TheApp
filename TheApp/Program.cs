9d86119c9dfe56f3c17ab381d6dd733c6a3d915d
Alexander2437 <alexander.vtyurin@gmail.com>
2024-11-21 22:31:35 +0700

    Тема 6.2. Классы и структуры.

--- TheApp/Program.cs
+++ TheApp/Program.cs
@@ -5,140 +5,141 @@ class Program
 {
     public static void Main(string[] args)
     {
-        var userInfo = GetUserData();
-        ShowUserData(userInfo);
+        //Human human = new Human();
+        //human.Greetings();
+
+        //human.name = "Иван";
+        //human.age = 40;
+        //human.Greetings();
+
+        //Human human = new Human();
+        //human.Greetings();
+
+        //Human human = new Human("Иван");
+        //human.Greetings();
+
+        //Human human = new Human("Иван", 45);
+        //human.Greetings();
+
+        //Animal animal = new Animal();
+
+        //animal.type = "dog";
+        //animal.name = "Spotty";
+        //animal.age = 5;
+
+        //animal.Info();
+
+        //Animal animal = new Animal("собака", "Шарик", 4);
+        //animal.Info();
+
+        //Animal animal = new Animal { type = "собака", name = "Вольт", age = 4 };
+        //animal.Info();
+
+        Rectangle rectangle = new Rectangle(8);
+        rectangle.CalculateSquare();
     }
+}
 
-    public static void ShowUserData((string name, string lastname, int age, bool hasPet, int petCount, string[] petNames, string[] favColors) UserData)
-        {
-            Console.WriteLine("Имя пользователя: {0}", UserData.name);
-            Console.WriteLine("Фамилия пользователя: {0}", UserData.lastname);
-            Console.WriteLine("Возраст пользователя: {0}", UserData.age);
-            
-            if (UserData.hasPet == true)
-            {
-                Console.WriteLine("Наличие питомца: да");
-                Console.WriteLine("Количество питомцев: {0}", UserData.petCount);
+class Rectangle
+{
+    public int a;
+    public int b;
 
-                for (int i = 0; i < UserData.petNames.Length; i++)
-                {
-                 Console.WriteLine("Имя питомца номер {0} - {1}", i + 1, UserData.petNames[i]);
-                }
-            }
-            else
-            {
-                Console.WriteLine("Наличие питомца: нет");
-            }
-
-            for (int i = 0; i < UserData.favColors.Length; i++)
-            {
-                Console.WriteLine("Любимый цвет номер {0} - {1}", i + 1, UserData.favColors[i]);
-            }
-    }
-    public static (string name, string lastname, int age, bool hasPet, int petCount, string[] petNames, string[] favColors) GetUserData()
+    public void CalculateSquare()
     {
-        var name = EnterData("имя пользователя");
-        var lastname = EnterData("фамилию пользователя");
-        int age = EnterNumber("возраст пользователя");
-       
-        Console.WriteLine("Наличие питомца. Укажите да или нет.");
-        bool hasPet = false;
-        string petAnswer = Console.ReadLine().ToUpper();
-        if (petAnswer == "Да".ToUpper())
-        {
-            hasPet = true;
-        }
-
-        int petCount = 0;
-        if (hasPet == true)
-        {
-            petCount = EnterNumber("количество питомцев");
-        }
-
-        string[] petNames = SetArray(petCount, "кличку питомца");
-
-        int favColorsNumber = EnterNumber("количество любимых цветов");
-
-        string[] favColors = SetArray(favColorsNumber, "любимый цвет");
-
-        (string name, string lastname, int age, bool hasPet, int petCount, string[] petNames, string[] favColors) UserData;
-
-        UserData.name = name;
-        UserData.lastname = lastname;
-        UserData.age = age;
-        UserData.hasPet = hasPet;
-        UserData.petCount = petCount;
-        UserData.petNames = petNames;
-        UserData.favColors = favColors;
-
-        return UserData;
+        Console.WriteLine($"Сторона a равна {a}, сторона b равна {b}, прощадь равна {a * b}.");
     }
 
-    private static string[] SetArray(int arraySize, string text)
+    public Rectangle(int side) 
     {
-        
-        string[] result = new string[arraySize];
-        for (int i = 0; i < arraySize; i++)
-        {
-            var arrayValue = EnterData(text);
-            result[i] = arrayValue;
-        }
-        return result;
+        a = side;
+        b = side;
     }
 
-    private static int EnterNumber(string text)
+    public Rectangle(int sideA, int sideB)
     {
-        bool isCorrectNumber = false;
-        int age;
-        do
-        {
-            Console.Write("Введите {0}: ", text);
-            string input = Console.ReadLine();
-            isCorrectNumber = CheckData(input, out age);
-        }
-        while (isCorrectNumber == false);
-        return age;
+        a = sideA;
+        b = sideB;
     }
 
-    private static string EnterData(string text)
+    public Rectangle()
     {
-        bool condition = false;
-        string name;
-        do
-        {
-            Console.Write("Введите {0}: ", text);
-            name = Console.ReadLine();
-            if (string.IsNullOrWhiteSpace(name))
-            {
-                condition = false;
-            }
-            else
-            {
-                condition = true;
-            }
-        }
-        while (condition == false);
-        return name;
+        a = 6;
+        b = 4;
+    }
+}
+
+class Human
+{
+    // Поля класса
+    public string name;
+    public int age;
+
+    // Метод класса
+    public void Greetings()
+    {
+        Console.WriteLine("Меня зовут {0}, мне {1}", name, age);
     }
 
-    public static bool CheckData(string number, out int corrnumber)
+    // Конструктор 1
+    public Human()
     {
-        bool result = false;
-        int x = 0;
-        if (int.TryParse(number, out int intnumber))
-        {
-            if (intnumber > 0)
-            {
-                x = intnumber;
-                result = true;
-            }
-        }
-        else
-        {
-            x = 0;
-            result = false;
-        }
-        corrnumber = x;
-        return result;
+        name = "Неизвестно";
+        age = 20;
+    }
+    // Конструктор 2
+    public Human(string n)
+    {
+        name = n;
+        age = 20;
+    }
+    // Конструктор 3
+    public Human(string n, int a)
+    {
+        name = n;
+        age = a;
+    }
+}
+
+struct Animal
+{
+    // Поля структуры
+    public string type;
+    public string name;
+    public int age;
+
+    // Метод структуры
+    public void Info()
+    {
+        Console.WriteLine("Это {0} по кличке {1}, ему {2}", type, name, age);
+    }
+
+    public Animal(string petType, string petName, int petAge)
+    {
+        type = petType;
+        name = petName;
+        age = petAge;
+    }
+}
+class Pen
+{
+    public string color;
+    public int cost;
+
+    public void ShowData()
+    {
+        Console.WriteLine("Цвет ручки: {0}.\nЦена ручки: {1}", color, cost);
+    }
+
+    public Pen()
+    {
+        color = "чёрный";
+        cost = 100;
+    }
+
+    public Pen(string penColor,  int penCost)
+    {
+        color = penColor;
+        cost = penCost;
     }
 }
