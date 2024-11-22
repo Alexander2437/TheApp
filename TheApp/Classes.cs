cde682a4c6272ff382da6a4182bc1de87286b32d
Alexander2437 <alexander.vtyurin@gmail.com>
2024-11-23 00:24:39 +0700

    Тема 6..3.1.

---
+++ TheApp/Classes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheApp
+{
+    class App1
+    {
+        Employee employee = new Employee();
+        //employee.Department 
+        //string CompanyName = employee.Department.Company.Name;
+    }
+    public class Employee
+    {
+        public Department Department;
+    }
+
+    public class Department
+    {
+        public Company Company;
+    }
+
+    public class Company
+    {
+        public string Name;
+    }
+}
--- TheApp/Program.cs
+++ TheApp/Program.cs
@@ -3,143 +3,37 @@ using System.Xml.Linq;
 
 class Program
 {
-    public static void Main(string[] args)
+    static void Main(string[] args)
     {
-        //Human human = new Human();
-        //human.Greetings();
+        Employee employee = new Employee();
+        
+        //Department department = new Department();
+        //employee.Department = department;
+        
+        //employee.Department = new Department();
 
-        //human.name = "Иван";
-        //human.age = 40;
-        //human.Greetings();
+        //employee.City = "Moscow";
 
-        //Human human = new Human();
-        //human.Greetings();
+        //string x = employee.City;
 
-        //Human human = new Human("Иван");
-        //human.Greetings();
+        //string companyname = employee.department.company.name;
 
-        //Human human = new Human("Иван", 45);
-        //human.Greetings();
-
-        //Animal animal = new Animal();
-
-        //animal.type = "dog";
-        //animal.name = "Spotty";
-        //animal.age = 5;
-
-        //animal.Info();
-
-        //Animal animal = new Animal("собака", "Шарик", 4);
-        //animal.Info();
-
-        //Animal animal = new Animal { type = "собака", name = "Вольт", age = 4 };
-        //animal.Info();
-
-        Rectangle rectangle = new Rectangle(8);
-        rectangle.CalculateSquare();
+        string name = employee.Department.Company.Name;
     }
 }
 
-class Rectangle
+public class Employee
 {
-    public int a;
-    public int b;
-
-    public void CalculateSquare()
-    {
-        Console.WriteLine($"Сторона a равна {a}, сторона b равна {b}, прощадь равна {a * b}.");
-    }
-
-    public Rectangle(int side) 
-    {
-        a = side;
-        b = side;
-    }
-
-    public Rectangle(int sideA, int sideB)
-    {
-        a = sideA;
-        b = sideB;
-    }
-
-    public Rectangle()
-    {
-        a = 6;
-        b = 4;
-    }
+    public Department Department;
+    public string City;
 }
 
-class Human
+public class Department
 {
-    // Поля класса
-    public string name;
-    public int age;
-
-    // Метод класса
-    public void Greetings()
-    {
-        Console.WriteLine("Меня зовут {0}, мне {1}", name, age);
-    }
-
-    // Конструктор 1
-    public Human()
-    {
-        name = "Неизвестно";
-        age = 20;
-    }
-    // Конструктор 2
-    public Human(string n)
-    {
-        name = n;
-        age = 20;
-    }
-    // Конструктор 3
-    public Human(string n, int a)
-    {
-        name = n;
-        age = a;
-    }
+    public Company Company;
 }
 
-struct Animal
+public class Company
 {
-    // Поля структуры
-    public string type;
-    public string name;
-    public int age;
-
-    // Метод структуры
-    public void Info()
-    {
-        Console.WriteLine("Это {0} по кличке {1}, ему {2}", type, name, age);
-    }
-
-    public Animal(string petType, string petName, int petAge)
-    {
-        type = petType;
-        name = petName;
-        age = petAge;
-    }
-}
-class Pen
-{
-    public string color;
-    public int cost;
-
-    public void ShowData()
-    {
-        Console.WriteLine("Цвет ручки: {0}.\nЦена ручки: {1}", color, cost);
-    }
-
-    public Pen()
-    {
-        color = "чёрный";
-        cost = 100;
-    }
-
-    public Pen(string penColor,  int penCost)
-    {
-        color = penColor;
-        cost = penCost;
-    }
+    public string Name;
 }
