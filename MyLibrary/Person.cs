9d3ed72422834fdb17f8f453f2716743e94c3001
Alexander2437 <alexander.vtyurin@gmail.com>
2024-12-13 23:49:14 +0700

    Тема 6.6. Добавление библиотеки классов.

---
+++ MyLibrary/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class Person
+    {
+        public string name;
+        //public Person(string name)
+        //{
+        //    this.name = name;
+        //}
+        public void Print() => Console.WriteLine($"Name: {name}");
+    }
+}
